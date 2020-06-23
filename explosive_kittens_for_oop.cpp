098087fff02830ee9023480e3a781b521859cea3
Baraev Sergey <Baraev.sergey@mail.ru>
2020-06-24 02:31:17 +0500

    Добавил метод на действия карт

--- explosive_kittens_for_oop.cpp
+++ explosive_kittens_for_oop.cpp
@@ -4,6 +4,8 @@
 #include "Player.h"
 
 using namespace std;
+
+void action_card(const vector<Player>& Players, Player Active_Player, int id_action);
 int main()
 {
     setlocale(LC_ALL, "Russian");
@@ -11,7 +13,7 @@ int main()
     vector <Card> deck; // вектор колоды
     vector <Player> active_players; // вектор игроков
     vector <Card> active_deck; // вектор активной колоды
-    int num_of_players=2; // переменная для задания количества игроков
+    int num_of_players = 2; // переменная для задания количества игроков
     int num_of_cards = 13; // количество карт в колоде
     int num_of_boom = num_of_players - 1;// количество взрывных котят в колод
     int num_of_norm = num_of_cards - num_of_boom; // количество "нейтральных" карт в колоде
@@ -35,6 +37,24 @@ int main()
         Card temp_card("Обычная карта", 0);
         deck.push_back(temp_card);
     }
+    action_card(active_players, active_players[0], 1);
     system("pause");
 }
 
+void action_card(const vector<Player>& Players, Player Active_Player, int id_action)
+{
+    switch (id_action)
+    {
+    case 0: //ничего
+        break;
+    case 1: //котенок
+    {
+        for (int i = 0; i != Players.size(); i++)
+        {
+            cout << 1;
+        }
+    }
+    default:
+        break;
+    }
+}
