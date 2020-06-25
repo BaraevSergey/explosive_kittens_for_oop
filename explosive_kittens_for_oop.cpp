041f6bf1ad093af19da8ce4a5dd866dce4c834e3
Валентин Гагаринов <vgagarinov99@outlook.com>
2020-06-25 22:17:35 +0500

    Методы

--- explosive_kittens_for_oop.cpp
+++ explosive_kittens_for_oop.cpp
@@ -4,8 +4,12 @@
 #include "Player.h"
 using namespace std;
 
+void action_card(vector<Player>& Players, Player Active_Player, Card card); // метод выполнения действий карт
+void create_player(int n, vector <Player>& active_players); // метод создания игроков
+void create_deck(int n_b, int n_n, vector <Card>& deck); // метод создания колоды
+void out_deck(vector <Card>& deck); //метод вывода колоды
+void out_players(vector <Player>& active_players); // метод вывода активных игроков
 
-void action_card(vector<Player>& Players, Player Active_Player, Card card);
 int main()
 {
     setlocale(LC_ALL, "Russian");
@@ -17,41 +21,10 @@ int main()
     int num_of_boom = num_of_players - 1;// количество взрывных котят в колод
     int num_of_norm = num_of_cards - num_of_boom; // количество "нейтральных" карт в колоде
     
-    for (int i = 0; i < num_of_players; i++) //создал цикл заполнения игроков
-    {
-        Player temp_player("Player"+to_string(i+1));
-        active_players.push_back(temp_player);
-    }
-
-    for (int i = 0; i < num_of_boom; i++) // присваивание свойств ВЗРЫВНЫМ картам
-    {
-        Card temp_card("Взрывная карта", 1);
-        deck.push_back(temp_card);
-    }
-
-    for (int i = 0; i < num_of_norm; i++) // присваивание свойств НЕЙТРАЛЬНЫМ картам
-
-    {
-        Card temp_card("Обычная карта", 0);
-        deck.push_back(temp_card);
-    }
-
-  
-
-    //метод вывода карт в колоде
-    cout << "Карты в колоде:\n";
-    for (Card card_deck : deck)
-    {
-        cout <<  card_deck.name+"\n";
-    }
-    //метод показа добавленных игроков
-    cout << "\nИгроки активные:\n";
-    for (Player player : active_players)
-    {
-        cout << player.name+"\n";
-    }
-
-
+    create_player(num_of_players, active_players);
+    create_deck(num_of_boom, num_of_norm, deck);
+    out_deck(deck);
+    out_players(active_players);
 
     while (active_players.size() != 1)
     {
@@ -77,6 +50,8 @@ int main()
         
 }
 
+// МНОГО МЕТОДОВ
+
 void action_card(vector<Player>& Players, Player Active_Player, Card card)
 //я создал метод тут , так как хз пока куда его
 {
@@ -99,21 +74,66 @@ void action_card(vector<Player>& Players, Player Active_Player, Card card)
     }
         break;
     case 1: //котенок
-    {
-        for (int i = 0; i != Players.size(); i++)
         {
-            if (Active_Player.name == Players[i].name)
+            for (int i = 0; i != Players.size(); i++)
             {
-                Players.erase(Players.begin() + i);
-            }
-            else
-            {
+                if (Active_Player.name == Players[i].name)
+                {
+                    Players.erase(Players.begin() + i);
+                }
+                else
+                {
 
-            }
+                }
 
         }
     }
     default:
         break;
     }
+}
+
+void create_player(int n, vector <Player>& active_players)
+{
+    for (int i = 0; i < n; i++) //создал цикл заполнения игроков
+    {
+        Player temp_player("Player" + to_string(i + 1));
+        active_players.push_back(temp_player);
+    }
+}
+
+void create_deck(int n_b, int n_n, vector <Card>& deck)
+{
+    for (int i = 0; i < n_b; i++) // присваивание свойств ВЗРЫВНЫМ картам
+    {
+        Card temp_card("Взрывная карта", 1);
+        deck.push_back(temp_card);
+    }
+
+    for (int i = 0; i < n_n; i++) // присваивание свойств НЕЙТРАЛЬНЫМ картам
+
+    {
+        Card temp_card("Обычная карта", 0);
+        deck.push_back(temp_card);
+    }
+}
+
+void out_deck(vector <Card>& deck)
+{
+    cout << "Карты в колоде:\n";
+    for (Card card_deck : deck)
+    {
+        cout << card_deck.name + "\n";
+    }
+}
+
+void out_players(vector <Player>& active_players)
+{
+    //метод показа добавленных игроков
+    cout << "\nИгроки активные:\n";
+    for (Player player : active_players)
+    {
+        cout << player.name + "\n";
+    }
+
 }
