4147bd43a938f17df912c09a4c29b0bba365b214
Artem Altyshev <Artem Altyshev@DESKTOP-91CNA8N>
2020-06-28 16:32:07 +0300

    debug/cpp & debug.h comments

--- debug.cpp
+++ debug.cpp
@@ -1,7 +1,12 @@
 #include "debug.h"
 
-void Debug::write_list_card_from_deck(vector <Card>& deck) //метод показыающий список карт в колоде
-{
+void Debug::write_list_card_from_deck(vector <Card>& deck)
+{	
+	/*
+	deck - набор различных карт
+	card_deck.name - название карт, находящихся в колоде
+	*/
+
     cout << "Карты в колоде:\n";
     for (Card card_deck : deck)
     {
@@ -9,9 +14,12 @@ void Debug::write_list_card_from_deck(vector <Card>& deck) //метод показыающий с
     }
 }
 
-void Debug::write_list_active_players(vector <Player>& active_players) //метод, который показывает какие игроки активные в списке есть
+void Debug::write_list_active_players(vector <Player>& active_players)
 {
-
+	/*
+	active_players - cписок активных игроков
+	player.name - имя активного игрока
+	*/
     cout << "\nИгроки активные:\n";
     for (Player player : active_players)
     {
@@ -20,8 +28,14 @@ void Debug::write_list_active_players(vector <Player>& active_players) //метод, 
     cout << "\n";
 }
 
-void Debug::write_list_card_from_players(Player player) //метод, показывающий карты в руке игрока
+void Debug::write_list_card_from_players(Player player)
 {
+	/*
+	player - игрок, карты которого мы показываем
+	player.name - имя активного игрока
+	player.hand - карты в руке игрока
+	card.name - название карты, которую берет игрок
+	*/
     cout << "У игрока " + player.name + " сейчас в руке:\n";
     for (Card card : player.hand)
     {
@@ -32,20 +46,38 @@ void Debug::write_list_card_from_players(Player player) //метод, показывающий ка
 
 void Debug::write_player_boom_info(Player player)
 {
+	/*
+	player - игрок, карты которого мы показываем
+	player.name - имя активного игрока
+	*/
 	cout << "Игрок " + player.name + " взорвался\n\n";
 }
 
 void Debug::write_player_winner_info(Player player)
 {
+	/*
+	player - игрок, карты которого мы показываем
+	player.name - имя активного игрока
+	*/
 	cout << "Игрок " + player.name + " победил!\n";
 }
 
 void Debug::write_took_card(Player player, Card card)
 {
+	/*
+	player - игрок, карты которого мы показываем
+	card - карта, которую мы показываем
+	player.name - имя активного игрока
+	card.name - название карты, которую берет игрок
+	*/
     cout << "Игрок " + player.name + " взял карту " + card.name + "\n";
 }
 
 void Debug::write_player_defuse_info(Player player)
 {
+	/*
+	player - игрок, карты которого мы показываем
+	player.name - имя активного игрока
+	*/
 	cout << "Игрок " + player.name + " сыграл карту обезвредить!\n";
 }
--- debug.h
+++ debug.h
@@ -7,6 +7,8 @@ using namespace std;
 
 class Debug
 {
+	/* Класс, описывающий методы вывода информации о событиях происходящих в игре */
+
 	public:
 		void write_list_card_from_deck(vector <Card>& deck); //метод показыающий список карт в колоде
 		void write_list_active_players(vector <Player>& active_players); //метод, который показывает какие игроки активные в списке есть
@@ -14,5 +16,5 @@ class Debug
 		void write_player_boom_info(Player player); //метод вывода информации о том, что игрок взорвался
 		void write_player_winner_info(Player player); //метод вывода информации о том, что игрок победил
 		void write_player_defuse_info(Player player); //метод, показывающий информацию о том, что игрок сыграл карту обезвредить
-		void write_took_card(Player player, Card card); //метод вывода инфы о том, что игрок взял карту
+		void write_took_card(Player player, Card card); //метод вывода информации о том, что игрок взял карту
 };
