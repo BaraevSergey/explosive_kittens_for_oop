d11a5600ee1d1bab44e50acd03aa2a3f9a077d13
Baraev Sergey <Baraev.sergey@mail.ru>
2020-06-28 16:27:33 +0500

    comments my part

--- explosive_kittens_for_oop.cpp
+++ explosive_kittens_for_oop.cpp
@@ -11,63 +11,62 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    setlocale(LC_ALL, "Russian");
-    vector <Card> deck; // вектор колоды
-    vector <Player> active_players; // вектор игроков
+    setlocale(LC_ALL, "Russian"); //задаем русскую локаль
+    vector <Card> deck; // вектор колоды карт
+    vector <Player> active_players; // вектор игроков активных
     //штука ввода параметров из консоли по гиту не передается, пока оставим 
     //int num_of_players = atoi(argv[1]); // переменная для задания количества игроков
-    int num_of_players = 3;
-    int num_of_cards = num_of_players * 6;// количество карт в колоде
-    int num_of_boom = num_of_players - 1;// количество взрывных котят в колод
-    int num_of_norm = num_of_cards - num_of_boom; // количество "нейтральных" карт в колоде
-	int num_of_defuse = num_of_players + 2;
+    int num_of_players = 3; //число игроков(можно менять)
+    int num_of_cards = num_of_players * 12;// количество карт в колоде которое будет
+    int num_of_boom = num_of_players - 1;// количество взрывных котят в колоде (игроки - 1 согласно правилам)
+    int num_of_defuse = num_of_players + 2; //количество карт обезвредить в игре(по правилам - игроков+2)
+    int num_of_norm = num_of_cards - num_of_boom- num_of_defuse; // количество карт без действия пока в колоде
+	
     
     Debug debug;
     Action action;
     Create create;
-    create.create_players(num_of_players, active_players);
-    create.create_card_neutral(num_of_norm, deck); 
-    create.create_card_bomb(num_of_boom, deck);
-	create.create_card_defuse(num_of_defuse, deck);
-    create.mix_deck(deck);
-    debug.write_list_card_from_deck(deck);
-    debug.write_list_active_players(active_players);
+    create.create_players(num_of_players, active_players); //создаем игроков
+    create.create_card_neutral(num_of_norm, deck);  //создаем карты без действия
+    create.create_card_bomb(num_of_boom, deck); //создаем бомбы
+	create.create_card_defuse(num_of_defuse, deck); //создаем карты обезвредить
+    create.mix_deck(deck); //размешиваем колоду карт
+    debug.write_list_card_from_deck(deck); //вывод информации о картах в колоде
+    debug.write_list_active_players(active_players); //вывод информации о созданных игрока
 
-    while (active_players.size() != 1)
+    while (active_players.size() != 1) //цикл игры основоной
     {
-        Sleep(300);
-        if (deck.back().id_action == 1)
+        Sleep(300); //пауза для наглядности
+        if (deck.back().id_action == 1) //если наверху колоды взрыв
         {
-            debug.write_took_card(active_players.back(), deck.back());
+            debug.write_took_card(active_players.back(), deck.back()); //вывод о том что игрок берет карту
             int i = 0; //счетчик какая карта в руке по счету
-            bool flag = true;
-            for (Card card : active_players.back().hand)
+            bool flag = true; //взрывается ли игрок
+            for (Card card : active_players.back().hand) //смотрим каждую карту в руке активного игрока
             {
-                if (card.id_action == 2)
+                if (card.id_action == 2) //если обезвредить
                 {
-                    debug.write_player_defuse_info(active_players.back());
-                    action.card_in_hand(deck, active_players, card, i, active_players.size() - 1);
-                    flag = false;
-                    debug.write_list_card_from_deck(deck);
-                    break;
+                    debug.write_player_defuse_info(active_players.back()); //играем её
+                    action.card_in_hand(deck, active_players, card, i, active_players.size() - 1); //обработка сыгрывания карты
+                    flag = false; //спаслись
+                    debug.write_list_card_from_players(active_players.front()); //вывод карт в руке игрока
+                    break; //перестаем смотреть карты
                 }
-                i++;
+                i++;//если не обезвредить то повысим счетчик
             }
-            if (flag)
+            if (flag) //если не спаслись
             {
-                action.get_card(active_players, deck.back(), active_players.size() - 1);
-                deck.pop_back();
-                debug.write_list_card_from_players(active_players.back());
+                action.get_card(active_players, deck.back(), active_players.size() - 1); //берем взрыв, умираем
+                deck.pop_back(); //удаляем взрыв из колоды
             }
         }
         else
         {
-            debug.write_took_card(active_players.back(), deck.back());
-            //берется карта, либо в руку кладется либо игрок умирает
-            action.get_card(active_players, deck.back(), active_players.size() - 1);
-            //ту карту которую взяли удаляем из колоды
-            deck.pop_back();
-            debug.write_list_card_from_players(active_players.back());
+            debug.write_took_card(active_players.back(), deck.back()); //берется карта
+            action.get_card(active_players, deck.back(), active_players.size() - 1); //событие взятия карты
+            deck.pop_back(); //убирается карта из колоды 
+            debug.write_list_card_from_players(active_players.front()); //вывод карт в руке игрока
+            
         }
     }
     debug.write_player_winner_info(active_players[0]);
